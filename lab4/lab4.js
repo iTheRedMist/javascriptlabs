35bc7b2eccffa74acc15658c99a9515458308dfa
iTheRedMist <brigadirchik1019@gmail.com>
2025-04-30 21:01:47 +0300

    Create lab4.js

---
+++ lab4/lab4.js
@@ -0,0 +1,57 @@
+//zavd1
+/*
+var temperature = 20
+if(temperature<=0){
+    console.log("Дуже холодно")
+} else if (temperature>0 && temperature <= 10){
+    console.log("Прохладно")
+} else if (temperature >= 11 && temperature <= 18){
+    console.log("Тепло")
+} else {
+    console.log("Жарко")
+}
+*/
+//zavd2
+/*
+let a = 5
+console.log(a % 2 == 0? "Число парне": "Число не парне")
+*/
+//zavd3
+/*
+let day = "П'ятниця"
+switch(day){
+    case "Понеділок":
+        console.log("Перший день тижня");
+        break
+    case "Вівторок":
+        console.log("Другий день тижня");
+        break
+    case "Середа":
+        console.log("Третій день тижня");
+        break
+    case "Четвер":
+        console.log("Четвертий день тижня");
+        break
+    case "П'ятниця":
+        console.log("П'ятий день тижня");
+        break
+    case "Субота":
+        console.log("Шостий день тижня");
+        break
+    case "Неділя":
+        console.log("Сьомий день тижня");
+        break
+    default:
+        console.log("Такого дня тижня не існує");
+        break
+}
+*/
+//zavd4
+/*
+let password = "admin123"
+if(password == "admin123"){
+    console.log("Доступ дозволено")
+} else {
+    console.log("Доступ заборонено")
+}
+*/
