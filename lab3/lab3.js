11741d46cbfd020591a305adba05956a80ad5de2
iTheRedMist <brigadirchik1019@gmail.com>
2025-04-30 20:31:09 +0300

    Create lab3.js

---
+++ lab3/lab3.js
@@ -0,0 +1,32 @@
+//zavd1-3
+/*
+let a = "wassup";
+let b = 5;
+let c = true;
+let d;
+let e = null;
+
+a = 1;
+b = "pussaw";
+d = false;
+
+console.log(a + b);
+*/
+
+//zavd4
+/*
+function func(value){
+    return typeof(value)
+}
+console.log(func(2));
+console.log(func("hello"));
+console.log(func(true));
+console.log(func(0));
+console.log(func('<='));
+console.log(func(null));
+*/
+//zavd5
+/*
+console.log(1 == '1')
+console.log(1 === '1')
+*/
