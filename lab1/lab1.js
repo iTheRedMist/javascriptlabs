407c3138f043e45ac19ff3f46cc7a4f2ff57e4d4
iTheRedMist <brigadirchik1019@gmail.com>
2025-04-30 19:21:15 +0300

    Update lab1.js

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -3,4 +3,6 @@ let a = "hello";
 const b = 5;
 console.log(typeof x);
 console.log(typeof a);
-console.log(typeof b);
+let student = { name: "Ім'я", age: 18, status: "Статус студента ", university: "Університет"};
+console.log(typeof student);
+console.log(student);
