798e89abdf84c073576a47499974648c252ae882
iTheRedMist <brigadirchik1019@gmail.com>
2025-05-22 15:51:49 +0300

    Create lab11.js

---
+++ lab11/lab11.js
@@ -0,0 +1,37 @@
+/*
+class User {
+    isEnter = false
+    start(){
+        console.log("Користувач увійшов!")
+        this.isEnter = true
+    }
+    stop(){
+        console.log("Користувач вийшов!")
+        this.isEnter = false
+    }
+}
+let user = new User
+console.log(user.isEnter)
+user.start
+console.log(user.isEnter)
+user.stop()
+console.log(user.isEnter)
+*/
+
+class User {
+    isEnter = false
+    start() {
+        console.log("Користувач увійшов!")
+        this.isEnter = true
+    }
+    stop() {
+        console.log("Користувач вийшов!")
+        this.isEnter = false
+    }
+}
+let user = new User
+console.log(user.isEnter)
+user.start()
+console.log(user.isEnter)
+user.stop()
+console.log(user.isEnter)
