e2bf64a6899e3e544be79dc4c68cd6cc1cf42933
iTheRedMist <brigadirchik1019@gmail.com>
2025-06-09 21:00:54 +0300

    Create lab21.js

---
+++ lab21/lab21.js
@@ -0,0 +1,16 @@
+
+function Student(name, grade) {
+  this.name = name;
+  this.grade = grade;
+}
+
+
+Student.prototype.showInfo = function() {
+  console.log(`Ім'я: ${this.name}, Оцінка: ${this.grade}`);
+};
+
+
+const student1 = new Student("Олег", 90);
+
+
+student1.showInfo();  
